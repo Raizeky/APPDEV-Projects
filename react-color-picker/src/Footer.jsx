0f93ce88d5fc62b93446675fc89f1a4f81d56590
doquilo_r <doquilo_r@f112.lan>
2024-09-25 12:59:18 +0800

    Complete work

---
+++ react-color-picker/src/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    //stateless component - no hooks, no funs=ctions, no states
+    return(
+        <footer>
+            <hr />
+            <p>&copy; {new Date().getFullYear()}MY COLOR PICKER Website || written by: Rainier Doquilo</p>
+        </footer>
+    )
+}
